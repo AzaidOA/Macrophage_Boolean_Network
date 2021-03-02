e39572b18d33048573bd13faaf645663f710e556
Manuel Azaid Ordaz Arias <azaid.ord@gmail.com>
2021-03-02 02:51:23 -0600

    I got the pheatmap of the attractors that are not labeled, and I have found that the IL6_out, NFKB and STAT3_IL6 nodes are never activeted

--- R/get_attractors.R
+++ R/get_attractors.R
@@ -1,8 +1,8 @@
 library('BoolNet')
 library('BoolNetPerturb')
 
-net <- loadNetwork("RedMacrofagos2_160221.csv")
-toSBML(net, "Macrophages_201017.sbml")
+net <- loadNetwork("./CSV/reduced_boolean_network.csv")
+toSBML(net, "./MODEL/reduced_model.sbml")
 net
 
 attr <- getAttractors(net, method = "sat.restricted", maxAttractorLength = 1,
@@ -11,8 +11,24 @@ attr
 
 
 
-labels <- read.csv("Macrophages_LabelRules_120121.csv")
-res <- labelAttractors(attr, labels, sep="#")
-unique(res)
+labels <- read.csv("./CSV/macrophage_label_rules.csv")
+cell_types <- labelAttractors(attr, labels, sep="#")
+unique(cell_types)
 
-write(res, 'AtractoresMalEtiquetados.txt')
+write(cell_types, './MODEL/labeled_attractors.txt')
+
+
+# Obteniendo atractores sin estado celular
+
+attr_df <- attractorToDataframe(attr, Boolean = T)[,-1:-2]
+attr_df <- cbind(attr_df, cell_types)
+onlyNoLabel <- attr_df[which("NoLabel" == cell_types),]
+
+genes_on <- c("IL12_out","IL6_out","STAT1","STAT5","NFKB","IRF3","STAT6",
+              "IRF4","ERK","STAT3_IL10","IL10_out","PPARG","STAT3_IL6",
+              "VEGF_out","HIF1a")
+onlyNoLabel <- onlyNoLabel[, genes_on]
+onlyNoLabel <- unique(onlyNoLabel)
+pheatmap(t(onlyNoLabel), scale = "none", color = c("red", "green"),
+         cluster_rows = F, cluster_cols = F)
+dev.off()
