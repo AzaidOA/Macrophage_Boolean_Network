bbb39afed18ed41a58bae91136056f80474124fb
Manuel Azaid Ordaz Arias <azaid.ord@gmail.com>
2021-03-09 14:37:03 -0600

    Initial commit

--- R/get_attractors.R
+++ R/get_attractors.R
@@ -1,17 +1,17 @@
 library('BoolNet')
 library('BoolNetPerturb')
-
-net <- loadNetwork("./CSV/reduced_boolean_network.csv")
+library("pheatmap")
+net <- loadNetwork("../CSV/reduced_boolean_network_proof.csv")
 toSBML(net, "./MODEL/reduced_model.sbml")
 net
 
-attr <- getAttractors(net, method = "sat.restricted", maxAttractorLength = 1,
+attr <- getAttractors(net, type="synchronous", method = "sat.restricted", maxAttractorLength = 1,
                       returnTable  = TRUE)
 attr
 
 
 
-labels <- read.csv("./CSV/macrophage_label_rules.csv")
+labels <- read.csv("../CSV/macrophage_label_rules_proof.csv")
 cell_types <- labelAttractors(attr, labels, sep="#")
 unique(cell_types)
 
@@ -24,11 +24,24 @@ attr_df <- attractorToDataframe(attr, Boolean = T)[,-1:-2]
 attr_df <- cbind(attr_df, cell_types)
 onlyNoLabel <- attr_df[which("NoLabel" == cell_types),]
 
-genes_on <- c("IL12_out","IL6_out","STAT1","STAT5","NFKB","IRF3","STAT6",
-              "IRF4","ERK","STAT3_IL10","IL10_out","PPARG","STAT3_IL6",
-              "VEGF_out","HIF1a")
+genes_on <- c("IL12_out","IL6_out","STAT1","STAT5","IRF3","STAT6",
+              "IRF4","ERK","STAT3_IL6",
+              "VEGF_out","HIF1a","FCGR","IL1R","TLR4","NFKB",
+              "KLF4","SOCS1","PPARG","STAT3_IL10","IL10_out","IL10_e")
+genes_on <- c("STAT6","ERK","FCGR","IL1R","TLR4",
+              "KLF4","SOCS1","PPARG","IL10_out","IL10_e","IL10R",
+              "STAT3_IL10","SOCS3","IL6_e","IL6R","NFKB","STAT3_IL6","IL6_out")
 onlyNoLabel <- onlyNoLabel[, genes_on]
 onlyNoLabel <- unique(onlyNoLabel)
-pheatmap(t(onlyNoLabel), scale = "none", color = c("red", "green"),
+pheatmap(t(onlyNoLabel), scale = "row", color = c("red", "green"),
          cluster_rows = F, cluster_cols = F)
 dev.off()
+attr_df <- attractorToDataframe(attr, Boolean = T)[,-1:-2]
+all <- attr_df
+g <- c("IL6_e","IL6R","STAT3_IL10","SOCS3","NFKB","STAT3_IL6","IL6_out")
+all <- all[, g]
+all <- unique(all)
+pheatmap(t(all), scale = "row", color = c("red", "green"),
+         cluster_rows = F, cluster_cols = F)
+dev.off()
+
